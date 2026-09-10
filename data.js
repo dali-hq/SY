91bfbd30d98313646d82529a20b083b93225f708
Mireille <9695669+dali-hq@users.noreply.github.com>
2026-09-10 16:53:05 +0800

    Update data.js

--- data.js
+++ data.js
@@ -326,7 +326,7 @@ const WORKER_DATA = [
   {
     "id": "34007",
     "name": "武玉純",
-    "photoId":1J6XTqVkBb9KAkWphdUvnGJ25LhmRTV2o7o7",
+    "photoId":"1J6XTqVkBb9KAkWphdUvnGJ25LhmRTV2o7o7",
     "photoUrl": "https://lh3.googleusercontent.com1J6XTqVkBb9KAkWphdUvnGJ25LhmRTV2o7Vo7"
   },
   {
